ba0f38a23f9aa4dddc0e775ccf558d0e328d5e4d
EC2 Default User <ec2-user@ip-172-31-10-35.ap-northeast-1.compute.internal>
2022-08-19 22:39:26 +0000

    model,controller,View,gem作成

---
+++ app/controllers/public/costomers_controller.rb
@@ -0,0 +1,18 @@
+class Public::CostomersController < ApplicationController
+  
+  def show
+  end
+  
+  def edit
+  end 
+  
+  def update
+  end
+  
+  def quit
+  end
+  
+  def withdrawal
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  
+  # 顧客用
+  devise_for :customers, skip:[:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  devise_for :admin, skip:[:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :public do
+    root to: "homes#top"
+    get 'about'=>"homes#about"
+    patch 'customers/withdraw/:id', to: 'customers#withdraw', as: "customers/withdraw"
+    resources :items, only:[:index, :show]
+    resources :customers, only:[:show, :edit, :update, :quit]
+    resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only:[:new, :check, :complete, :create, :index, :show]
+    resources :deliveries, only:[:index, :edit, :create, :update, :destroy]
+  end
+  
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+    resources :order_items, only:[:update]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

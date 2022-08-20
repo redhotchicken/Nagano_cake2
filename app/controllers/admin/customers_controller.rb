668072c0990167f53480631623fb1c3aadcbba23
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-20 07:39:06 +0000

    [add]会員ステータス以外のview追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,9 +7,18 @@ class Admin::CustomersController < ApplicationController
   end
   
   def edit
+    @customer = Customer.find(params[:id])
   end
   
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to riquest.referer
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_kana, :first_kana, :post_code, :address, :tell, :email, :is_deleted)
   
 end

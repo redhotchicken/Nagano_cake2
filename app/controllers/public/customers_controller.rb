a33182c87dc2eccb4f3a8c8655529abf9fedbdde
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-20 06:50:41 +0000

    [add]bootstrapとfontawesomeを入れました

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,26 @@
-class Public::CostomersController < ApplicationController
-  
+class Public::CustomersController < ApplicationController
+
   def show
+    @customer = current_customer
   end
-  
+
   def edit
-  end 
-  
+  end
+
   def update
   end
-  
+
   def quit
   end
-  
+
   def withdrawal
   end
-  
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_kana, :first_kana, :post_code, :address, :tell, :email)
+  end
+
 end
+

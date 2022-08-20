82d9631e8b4cb2cc9524db2ac17cf14371063b44
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-20 08:55:47 +0000

    確認しました

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -8,7 +8,7 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     public_root_path
   end
-  
+
   def after_sign_out_path_for(resource)
     public_root_path
   end

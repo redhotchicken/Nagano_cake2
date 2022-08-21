f09d69b3bdf34854508573bd66113341ae8c7c11
EC2 Default User <ec2-user@ip-172-31-10-35.ap-northeast-1.compute.internal>
2022-08-21 22:13:50 +0000

    商品index,show,edit作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,18 @@ class Item < ApplicationRecord
   validates :name, presence:true
   validates :description, presence:true
   validates :price, presence:true
-  validates :is_active, presence:true
+  #validates :is_active, presence:true
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type:'image/jpeg')
+    end
+      image.variant(resize_to_fit:[width, height]).processed
+  end
+  
+  def tax_price
+    (price*1.1).floor
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,135 +11,143 @@
 Genre.create!(name:"ケーキ")
 Genre.create!(name:"プリン")
 Genre.create!(name:"焼き菓子")
-Genre.create!(name: "キャンディ")
+Genre.create!(name:"キャンディ")
 
 #商品情報
-Product.create!(
+Item.create!(
   name: "洋梨のチーズタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
+  description: "甘い洋梨をふんだんに使用したチーズタルトです。",
+  price: "1200",
   genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+  is_active: true,
   #image: open("")
 )
-Product.create!(
+Item.create!(
   name: "ザッハトルテ",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
+  description: "スポンジ内部にあんずジャムを塗り重ね、チョコレートフォンダンはしっとり滑らかな食感に仕上げました。",
+  price: "2900",
   genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+  is_active: true,
   #image: open("")
 )
-Product.create!(
+Item.create!(
+  name: "アイスキャンディー",
+  description: "ケーキ屋が作った、口当たり滑らかなアイスキャンディーです。",
+  price: "800",
+  genre_id: 4,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
   name: "モンブラン",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "卵たっぷり濃厚プリン",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "紅茶のクッキー",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "フルーツタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "かぼちゃのプリン",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "マカロン",
-  description: "高級感のあるマカロン",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "マカロンセット（8個）",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "マカロンセット（16個）",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "バナナケーキ",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
-  #image: open("")
-)
-Product.create!(
-  name: "ショートケーキ",
-  description: "甘くて美味しいイチゴのショートケーキ",
+  description: "渋皮マロンを使用し、栗本来の味が楽しめます！",
   price: "500",
   genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+  is_active: true,
   #image: open("")
 )
-Product.create!(
-  name: "洋梨のチーズタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+Item.create!(
+  name: "卵たっぷり濃厚プリン",
+  description: "プリン好きにはたまらない納得のプリンです！",
+  price: "500",
+  genre_id: 2,
+  is_active: true,
   #image: open("")
 )
-Product.create!(
-  name: "洋梨のチーズタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
-  genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+Item.create!(
+  name: "紅茶のクッキー",
+  description: "オリジナルブレンドでほっと一息",
+  price: "800",
+  genre_id: 3,
+  is_active: true,
   #image: open("")
 )
-Product.create!(
-  name: "洋梨のチーズタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
+Item.create!(
+  name: "フルーツタルト",
+  description: "9種類のフルーツを使用し、おいしさをギュギュッとつめこみました！",
+  price: "2900",
   genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+  is_active: false,
   #image: open("")
 )
-Product.create!(
-  name: "洋梨のチーズタルト",
-  description: "甘い洋梨をふんだんに使用したチーズタルト",
-  price: "1,200",
+Item.create!(
+  name: "かぼちゃのプリン",
+  description: "かぼちゃをふんだんに使用し、口当たりなめらかに仕上げました。",
+  price: "480",
+  genre_id: 2,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "マカロン",
+  description: "高級感のあるマカロン",
+  price: "200",
+  genre_id: 3,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "マカロンセット（8個）",
+  description: "よりどりマカロン8個セット",
+  price: "1200",
+  genre_id: 3,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "マカロンセット（16個）",
+  description: "よりどりマカロン16個セット",
+  price: "2000",
+  genre_id: 3,
+  is_active: false,
+  #image: open("")
+)
+Item.create!(
+  name: "バナナケーキ",
+  description: "農家直送バナナを使用したショートケーキ",
+  price: "980",
   genre_id: 1,
-  is_status: [['販売中', true], ['販売停止中', false]],
+  is_active: false,
+  #image: open("")
+)
+Item.create!(
+  name: "シュークリーム",
+  description: "カスタードクリームをたっぷり使用した食べごたえ抜群のシュークリームです！！",
+  price: "480",
+  genre_id: 1,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "ミルフィーユ",
+  description: "サクサクなパイ生地に濃厚なクリームをはさんだ、自慢の逸品！！",
+  price: "700",
+  genre_id: 1,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "チーズケーキ",
+  description: "濃くて美味しい、手軽で嬉しい",
+  price: "580",
+  genre_id: 1,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "ホールケーキ",
+  description: "みんなで分け合うまぁるいしあわせ",
+  price: "3200",
+  genre_id: 1,
+  is_active: true,
+  #image: open("")
+)
+Item.create!(
+  name: "チョコレートケーキ(ホール)",
+  description: "ビターなチョコレートを使用した大人のホールケーキ",
+  price: "3800",
+  genre_id: 1,
+  is_active: false,
   #image: open("")
 )
 

ef4a3fd313935ff137530dc2f625bdb9231d9587
43962 <qianqinggongben@gmail.com>
2022-08-22 05:57:36 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 Rails.application.routes.draw do
-  
+
   # 顧客用
   devise_for :customers, skip:[:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   devise_for :admin, skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :public do
     root to: "homes#top"
     get 'about'=>"homes#about"
     patch 'customers/withdrawal/:id', to: 'customers#withdrawal', as: "customers/withdrawal"
+    get 'orders/complete', to: 'orders#complete', as: "orders/complete"
+    post 'orders/check', to: 'orders#check', as: "orders/check"
     resources :items, only:[:index, :show]
     resources :customers, only:[:show, :edit, :update, :quit]
     resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
-    resources :orders, only:[:new, :check, :complete, :create, :index, :show]
+    resources :orders, only:[:new, :create, :index, :show]
     resources :deliveries, only:[:index, :edit, :create, :update, :destroy]
   end
-  
+
   namespace :admin do
     root to: "homes#top"
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
@@ -30,6 +32,6 @@ Rails.application.routes.draw do
     resources :orders, only:[:show, :update]
     resources :order_items, only:[:update]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

204f5001b951601eda42300e19a19948f1cdd65a
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-22 04:10:43 +0000

    確認しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  
+
   # 顧客用
   devise_for :customers, skip:[:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   devise_for :admin, skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :public do
     root to: "homes#top"
     get 'about'=>"homes#about"
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
     resources :orders, only:[:new, :check, :complete, :create, :index, :show]
     resources :deliveries, only:[:index, :edit, :create, :update, :destroy]
   end
-  
+
   namespace :admin do
     root to: "homes#top"
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
@@ -30,6 +30,6 @@ Rails.application.routes.draw do
     resources :orders, only:[:show, :update]
     resources :order_items, only:[:update]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

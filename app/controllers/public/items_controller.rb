a31b40788923cb1ebb6be2c2b8eb9281169dde0f
43962 <qianqinggongben@gmail.com>
2022-08-22 05:44:38 +0000

    商品詳細修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,22 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
    @items = Item.page(params[:page])
   end
-  
+
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new(params[:cart_item_params])
   end
-  
+
   private
-  
-　def item_params
-  　params.require(:item).permit(:item_image, :name, :price)
-　end
-  
+
+ def item_params
+   params.require(:item).permit(:item_image, :name, :price)
+ end
+ 
+ def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+ end
+
 end

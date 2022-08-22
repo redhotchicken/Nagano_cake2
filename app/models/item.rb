e98a2ad21a21bcd859e4486d6e7c2a63066e9dba
43962 <qianqinggongben@gmail.com>
2022-08-22 02:18:34 +0000

    商品一覧エラー解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
 
   has_many:cart_items, dependent: :destroy
-
+  has_one_attached:item_image
+  
    #消費税を求めるメソッド
   def with_tax_price
     (price*1.1).floor
   end
 
+  def get_item_image
+    (item_image.attached?) ? item_image : 'no_image.jpg'
+  end
 end

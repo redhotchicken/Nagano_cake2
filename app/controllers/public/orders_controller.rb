c8c269d953e02e6c0fa4318959d9a890c3ed8e05
43962 <qianqinggongben@gmail.com>
2022-08-23 08:11:50 +0000

    注文情報入力画面修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,36 +1,50 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
   end
-  
+
   def check
+    @cart_items = current_customer.cart_items
     @order = Order.new
-  
-    @order.post_code = current_customer.post_code
-    @order.address = current_customer.address
-    @order.name = current_customer.name
-    @order.payment_type = current_customer.payment_type
+     if params[:order][:select_address] == "0"
+        @order = Order.new(order_params)
+        @order.post_code = current_customer.post_code
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name
+
+     elsif params[:order][:select_address] == "1"
+       @order = Order.new(order_params)
+       @address = Delivery.find(params[:order][:delivery_id])
+       @order.post_code = @delivery.post_code
+       @order.address = @delivery.address
+       @order.name = @delivery.name
+
+     elsif params[:order][:select_address] == "2"
+       @order = Order.new(order_params)
+       @order.save
+     end
+
   end
-  
+
   def complete
   end
-  
+
   def create
      @order = Order.new(order_params)
      @order = Order.save
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:payment_type, :post_code, :address, :name, :customer_id)
   end
-  
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ has_many:cart_items, dependent: :destroy
 belongs_to:customer
 
 enum payment_type: { credit_card: 0, transfer: 1 }
-enum address: { customer_address: 0, registered_address: 1, new_address: 2 }
 
 
 

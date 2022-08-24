fa1eafac4ccdefa6e3591c3f575114619576ef88
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-24 11:46:04 +0000

    リンクの修正と同一商品を追加した時個数だけ増えるように修正しました

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -27,10 +27,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to public_cart_items_path
+      if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+         cart_item.amount += params[:cart_item][:amount].to_i
+         cart_item.save
+         redirect_to public_cart_items_path
+      else
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_customer.id
+        @cart_item.save
+        redirect_to public_cart_items_path
+      end
   end
 
   private

33c17f6d30ce2ebd8db2d8c68559fb8cb4dca248
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-24 01:08:56 +0000

    確認

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,10 +9,14 @@ class Public::CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.customer_id = current_customer.id
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-    @cart_item.amount += params[:cart_item][:amount].to_i
+    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+    @cart_item.amount += params[:amount].to_i
     @cart_item.update
+    redirect_to public_cart_items_path
+    else 
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
     redirect_to public_cart_items_path
     end
   end

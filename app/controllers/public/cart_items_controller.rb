ab5d02e604f0119b0a7761da40a2eeb2e6f8e3f6
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-08-24 06:33:14 +0000

    [add] カート機能修正しました

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,22 +3,14 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
     @cart_items = current_customer.cart_items
-    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @total = 0
+
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.customer_id = current_customer.id
-    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
-    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
-    @cart_item.amount += params[:amount].to_i
-    @cart_item.update
+    @cart_item.update(cart_item_params)
     redirect_to public_cart_items_path
-    else 
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
-    redirect_to public_cart_items_path
-    end
   end
 
   def destroy

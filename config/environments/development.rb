b62b8d652e18ce8f75668a8a7bd02b4e099ace1c
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-25 05:24:18 +0000

    注文情報pull

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
+  config.hosts << "e59df6124e374e69a78da814626dad0f.vfs.cloud9.us-east-1.amazonaws.com"
   config.hosts << ".vfs.cloud9.ap-northeast-1.amazonaws.com"
 
 end

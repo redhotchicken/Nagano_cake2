9e9d0423b22ff9dd6701c088b84def219e18e26d
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-25 08:57:29 +0000

    エラー修正中

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,19 @@
 class Order < ApplicationRecord
 
-belongs_to :customer
-has_many :order_items
-
-enum payment_type: { credit_card: 0, transfer: 1 }
-enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, preparing_to_ship: 3, sent:4 }
-
-# 小計を求めるメソッド
-#消費税を求めるメソッドwith_tax_priceと掛け合わせる
-#商品の税込み価格*個数
-def subtotal
-    item.with_tax_price * amount
-end
-
-
+  belongs_to :customer
+  has_many :order_items
+  
+  enum payment_type: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, preparing_to_ship: 3, sent:4 }
+  
+  # 小計を求めるメソッド
+  #消費税を求めるメソッドwith_tax_priceと掛け合わせる
+  #商品の税込み価格*個数
+  def with_tax_price
+      (price*1.1).floor
+  end
+  def subtotal
+      item.with_tax_price * amount
+  end
+  
 end
